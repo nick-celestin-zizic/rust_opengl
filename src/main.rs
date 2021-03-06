4c4bf90f587ae1202ca7f8dcbf24819a913b5f05
Nick C. Zizic <nix509@gmail.com>
2021-03-06 00:43:08 -0500

    specular lighting works

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ extern crate gl;
 use gl::glutin as initgl;
 
 mod types;
-use types::{Vertex, Matrix4};
-
+use types::*;
 
 fn compile_shader (display : &gl::Display,
                    name : &str) -> gl::program::Program {
@@ -50,12 +49,14 @@ fn load_model (display : &gl::Display,
     
     let index_buffer = gl::IndexBuffer::new(
         display, gl::index::PrimitiveType::TrianglesList, &indices)
-                      .expect("failed to create index buffer");
+        .expect("failed to create index buffer");
 
     (vertex_buffer, index_buffer)
 }
 
 fn main () {
+    gl::implement_vertex!(Vertex, position, normal, texture);
+
     let (display, event_loop) = {
         use initgl::{ event_loop::EventLoop,
                       window::WindowBuilder,
@@ -66,33 +67,30 @@ fn main () {
         let window     = WindowBuilder::new()
             .with_title("GABAGOL")
             .with_inner_size(LogicalSize::new(400, 600));
-        let context = ContextBuilder::new();
+        let context = ContextBuilder::new()
+            .with_depth_buffer(24);
         let display = gl::Display::new(window, context, &event_loop)
             .expect("failed to create display");
-
+        
         (display, event_loop)
-    };
-
-    gl::implement_vertex!(Vertex, position, normal, texture);
-
-    let _params = gl::DrawParameters {
-        depth : gl::Depth {
-            test  : gl::draw_parameters::DepthTest::IfLess,
-            write : true,
-            .. Default::default()
-        },
-        .. Default::default()
     };
 
 
     let (vertex_buffer, index_buffer) = load_model(&display, "teapot");
-    let triangle_shader = compile_shader(&display, "basic");
-    let projection : Matrix4 = [
-        [ 1.0,  0.0,  0.0,  0.0],
-        [ 0.0,  1.0,  0.0,  0.0],
-        [ 0.0,  0.0,  1.0, 0.0],
-        [ 0.0,  0.0,  0.0,  1.0]
+    let basic_shader = compile_shader(&display, "basic");
+
+    let color : Vector3 = [1.0, 0.0, 0.0];
+    let light : Vector3 = [-1.0, 0.4, 0.9];
+    let model : Matrix4 = [
+        [ 3.0,  0.0,  0.0,  0.0],
+        [ 0.0,  3.0,  0.0,  0.0],
+        [ 0.0,  0.0,  1.0,  0.0],
+        [ 0.0,  0.0,  2.0,  1.0]
     ];
+    let view = Matrix4::view([ 2.0, -1.0,  1.0],
+                             [-2.0,  1.0,  1.0],
+                             [ 0.0,  1.0,  0.0]);
+
     //let mut t : f32 = 0.01;
     event_loop.run(move |event, _, control_flow| {
         use initgl::event_loop::ControlFlow::*;
@@ -125,20 +123,47 @@ fn main () {
             _ => ()
         }
         
-        
-
-        //t += 0.001;
-
+        //t += 0.01;
         
         use gl::Surface;
         let mut frame = display.draw();
-        frame.clear_color(0.0, 0.0, 0.0, 1.0);
+        frame.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+
+        let params = gl::DrawParameters {
+            depth: gl::Depth {
+                test: gl::draw_parameters::DepthTest::IfLess,
+                write: true,
+                .. Default::default()
+            },
+            //backface_culling : gl::draw_parameters::BackfaceCullingMode::CullClockwise,
+            .. Default::default()
+        };
+
+        let perspective = {
+            let (width, height) = frame.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
+
+            let fov : f32 = 3.141592 / 3.0;
+            let zfar  = 1024.0;
+            let znear = 0.1;
+
+            let f = 1.0 / (fov / 2.0).tan();
+
+            [[f *   aspect_ratio, 0.0,              0.0              , 0.0],
+             [         0.0      ,  f ,              0.0              , 0.0],
+             [         0.0      , 0.0,  (zfar+znear)/(zfar-znear)    , 1.0],
+             [         0.0      , 0.0, -(2.0*zfar*znear)/(zfar-znear), 0.0]]
+        };
         
         frame.draw(&vertex_buffer,
                    &index_buffer,
-                   &triangle_shader,
-                   &gl::uniform! {matrix : projection},
-                   &Default::default()).expect("triangle fucked up");
+                   &basic_shader,
+                   &gl::uniform! { u_color       : color,
+                                   u_light       : light,
+                                   u_model       : model,
+                                   u_view        : view,
+                                   u_perspective : perspective },
+                   &params).expect("triangle fucked up");
         
         frame.finish().expect("could not swap buffers");
     });
